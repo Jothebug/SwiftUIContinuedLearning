104e364ea71cbf76865962378d1c167b3e7f5da1
Jothebug <hayenbg2603@gmail.com>
2024-09-29 15:19:15 +0700

    Initial Commit

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by HaYen on 29/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
