25d8f292fb3a883f4e20f6e10c068a2ab9b17cb3
Jothebug <hayenbg2603@gmail.com>
2024-10-01 23:44:15 +0700

    DragGestureBootcamp

---
+++ SwiftUIContinuedLearning/DragGestureBootcamp.swift
@@ -0,0 +1,52 @@
+//
+//  DragGestureBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by HaYen on 1/10/24.
+//
+
+import SwiftUI
+
+struct DragGestureBootcamp: View {
+    
+    @State var offset: CGSize = .zero
+    
+    var body: some View {
+        ZStack {
+            
+            VStack {
+                Text("\(offset.width)")
+                Spacer()
+            }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 500)
+                .offset(offset)
+                .scaleEffect(getScaleAmount())
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.spring()) {
+                                offset = value.translation
+                            }
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                offset = .zero
+                            }
+                        }
+            )
+        }
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - min(percentage, 0.5) * 0.5
+    }
+}
+
+#Preview {
+    DragGestureBootcamp()
+}
