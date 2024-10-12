db7baeae5d0fa14ee866f95937e7e5375c913c1c
Jothebug <hayenbg2603@gmail.com>
2024-10-12 15:51:25 +0700

    GeometryReaderBootcamp

---
+++ SwiftUIContinuedLearning/GeometryReaderBootcamp.swift
@@ -0,0 +1,49 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by HaYen on 10/10/24.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).minX
+        return Double(1 - (currentX / maxDistance))
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 40),
+                                axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+//        GeometryReader { geometry in
+//            HStack(spacing: 0) {
+//                Rectangle()
+//                    .fill(Color.red)
+//                    .frame(width: geometry.size.width * 0.6666)
+//                
+//                Rectangle().fill(Color.green)
+//            }
+//            .ignoresSafeArea()
+//        }
+    }
+}
+
+#Preview {
+    GeometryReaderBootcamp()
+}
