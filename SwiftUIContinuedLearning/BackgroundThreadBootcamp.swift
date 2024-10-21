4458acc7443628c1a7e69f4f62c46b2a823c6d51
Jothebug <hayenbg2603@gmail.com>
2024-10-21 23:06:35 +0700

    BackgroundThreadBootcamp

---
+++ SwiftUIContinuedLearning/BackgroundThreadBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by HaYen on 21/10/24.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("1: \(Thread.current)")
+            print("1: \(Thread.isMainThread)")
+            
+            DispatchQueue.main.async {
+                print("2: \(Thread.current)")
+                print("2: \(Thread.isMainThread)")
+                self.dataArray = newData
+            }
+        }
+    }
+    
+    func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+    
+}
+
+struct BackgroundThreadBootcamp: View {
+    
+    @StateObject private var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                        Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    BackgroundThreadBootcamp()
+}
