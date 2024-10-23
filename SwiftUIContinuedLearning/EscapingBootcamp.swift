95036b7a51162d56cb743e608b7f797cc0bf5c55
Jothebug <hayenbg2603@gmail.com>
2024-10-23 23:01:20 +0700

    EscapingBootcamp

---
+++ SwiftUIContinuedLearning/EscapingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  EscapingBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by HaYen on 23/10/24.
+//
+
+import SwiftUI
+
+struct EscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EscapingBootcamp()
+}
