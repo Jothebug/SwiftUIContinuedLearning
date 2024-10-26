a4183b7cc3ee9cf06a092397e13c8b2b91e4d3be
Jothebug <hayenbg2603@gmail.com>
2024-10-26 19:30:12 +0700

    EscapingBootcamp

--- SwiftUIContinuedLearning/EscapingBootcamp.swift
+++ SwiftUIContinuedLearning/EscapingBootcamp.swift
@@ -7,9 +7,63 @@
 
 import SwiftUI
 
+class EscapingViewModel: ObservableObject {
+    
+    @Published var text: String = "Hello"
+    
+    func getData() {
+        downloadData5 {[weak self] returnedData in
+            self?.text = returnedData.data
+        }
+    }
+    
+    func downloadData() -> String {
+        return "New data!"
+    }
+    
+    func downloadData2(completionHandler: (_ data: String) -> Void) {
+        completionHandler("New Data!")
+    }
+    
+    func downloadData3(completionHandler: @escaping (_ data: String) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            completionHandler("New data download 3")
+        }
+    }
+    
+    func downloadData4(completionHandler: @escaping (DownloadResult) -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New data download 4")
+            completionHandler(result)
+        }
+    }
+    
+    func downloadData5(completionHandler: @escaping DownloadCompletion) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let result = DownloadResult(data: "New data download 5")
+            completionHandler(result)
+        }
+    }
+    
+}
+
+struct DownloadResult {
+    let data: String
+}
+typealias DownloadCompletion = (DownloadResult) -> ()
+
 struct EscapingBootcamp: View {
+    
+    @StateObject var vm = EscapingViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(vm.text)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(.blue)
+            .onTapGesture {
+                vm.getData()
+            }
     }
 }
 
