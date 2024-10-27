d966f1d9a9534314305e04c5f2fb3abb52c25c32
Jothebug <hayenbg2603@gmail.com>
2024-10-27 15:46:56 +0700

    DownloadWithCombineBootcamp

---
+++ SwiftUIContinuedLearning/DownloadWithCombineBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithCombineBootcamp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by HaYen on 27/10/24.
+//
+
+import SwiftUI
+
+struct DownloadWithCombineBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithCombineBootcamp()
+}
